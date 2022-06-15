22cbec575e54d0887f42d78a1805ea7c7429ea39
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-06-15 14:17:27 +0300

    solve the problem with catching errors

--- 1. Asynchronous Programming/01.XHR/scripts/app.js
+++ 1. Asynchronous Programming/01.XHR/scripts/app.js
@@ -1,18 +1,34 @@
 function loadRepos() {
-  const div = document.getElementById("res");
+  const username = document.getElementById("username");
+  const repos = document.getElementById("repos");
 
-  let url = "https://api.github.com/users/testnakov/repos";
+  const url = `https://api.github.com/users/${username.value}/repos`;
 
-  const httpRequest = new XMLHttpRequest();
+  fetch(url)
+    .then((response) => {
+      repos.innerHTML = "";
+      if (!response.ok) {
+        throw new Error("404 Not Found");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      repos.innerHTML = "";
 
-  httpRequest.addEventListener("readystatechange", () => {
-    if (httpRequest.readyState == 4 && httpRequest.status === 200) {
-      let data = httpRequest.responseText;
+      data.forEach((element) => {
+        const liElement = document.createElement("li");
+        const anchorElement = document.createElement("a");
 
-      div.textContent = data;
-    }
-  });
+        anchorElement.textContent = element.full_name;
+        anchorElement.setAttribute("href", element.html_url);
 
-  httpRequest.open('GET', url);
-  httpRequest.send();
+        liElement.appendChild(anchorElement);
+        repos.appendChild(liElement);
+      });
+    })
+    .catch((err) => {
+      const liElement = document.createElement("li");
+      liElement.textContent = `${err}`;
+      repos.appendChild(liElement);
+    });
 }
