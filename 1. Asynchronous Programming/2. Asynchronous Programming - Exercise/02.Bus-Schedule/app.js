fe9fb26e5e4a05f2e7e64ecc903249c4a6f437f8
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-06-16 13:35:28 +0300

    Adding solved bus-schedule problem

---
+++ 1. Asynchronous Programming/2. Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
@@ -0,0 +1,47 @@
+function solve() {
+  const info = document.getElementById("info").firstChild;
+  const url = "http://localhost:3030/jsonstore/bus/schedule/";
+
+  const departBtn = document.getElementById("depart");
+  const arriveBtn = document.getElementById("arrive");
+
+  let nextStop = "depot";
+  let arriving = "";
+
+  function depart() {
+    info.textContent = 'Loading ...';
+    fetch(`${url}/${nextStop}`)
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error();
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        info.textContent = `Next stop ${data.name}`;
+        arriving = data.name;
+        nextStop = data.next;
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
+      })
+      .catch((err) => {
+        info.textContent = `${err}`;
+        departBtn.disabled = true;
+        arriveBtn.disabled = true;
+      });
+  }
+
+  function arrive() {
+    info.textContent = `Arriving at ${arriving}`;
+    departBtn.disabled = false;
+    arriveBtn.disabled = true;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
+}
+
+let result = solve();
