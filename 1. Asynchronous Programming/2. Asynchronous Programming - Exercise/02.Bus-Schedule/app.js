847b75312b2a073560c0f91b935c2ab3382878e2
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-06-16 14:06:45 +0300

    Solved bus-schedule problem with async/await

--- 1. Asynchronous Programming/2. Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
+++ 1. Asynchronous Programming/2. Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
@@ -8,28 +8,27 @@ function solve() {
   let nextStop = "depot";
   let arriving = "";
 
-  function depart() {
-    info.textContent = 'Loading ...';
-    fetch(`${url}/${nextStop}`)
-      .then((response) => {
-        if (response.status !== 200) {
-          throw new Error();
-        }
+  async function depart() {
+    info.textContent = "Loading ...";
 
-        return response.json();
-      })
-      .then((data) => {
-        info.textContent = `Next stop ${data.name}`;
-        arriving = data.name;
-        nextStop = data.next;
-        departBtn.disabled = true;
-        arriveBtn.disabled = false;
-      })
-      .catch((err) => {
-        info.textContent = `${err}`;
-        departBtn.disabled = true;
-        arriveBtn.disabled = true;
-      });
+    try {
+      const response = await fetch(`${url}/${nextStop}`);
+
+      if (response.status !== 200) {
+        throw new Error();
+      }
+
+      const data = await response.json();
+      info.textContent = `Next stop ${data.name}`;
+      arriving = data.name;
+      nextStop = data.next;
+      departBtn.disabled = true;
+      arriveBtn.disabled = false;
+    } catch (err) {
+      info.textContent = `${err}`;
+      departBtn.disabled = true;
+      arriveBtn.disabled = true;
+    }
   }
 
   function arrive() {
@@ -45,3 +44,51 @@ function solve() {
 }
 
 let result = solve();
+
+// function solve() {
+//   const info = document.getElementById("info").firstChild;
+//   const url = "http://localhost:3030/jsonstore/bus/schedule/";
+
+//   const departBtn = document.getElementById("depart");
+//   const arriveBtn = document.getElementById("arrive");
+
+//   let nextStop = "depot";
+//   let arriving = "";
+
+//   function depart() {
+//     info.textContent = 'Loading ...';
+//     fetch(`${url}/${nextStop}`)
+//       .then((response) => {
+//         if (response.status !== 200) {
+//           throw new Error();
+//         }
+
+//         return response.json();
+//       })
+//       .then((data) => {
+//         info.textContent = `Next stop ${data.name}`;
+//         arriving = data.name;
+//         nextStop = data.next;
+//         departBtn.disabled = true;
+//         arriveBtn.disabled = false;
+//       })
+//       .catch((err) => {
+//         info.textContent = `${err}`;
+//         departBtn.disabled = true;
+//         arriveBtn.disabled = true;
+//       });
+//   }
+
+//   function arrive() {
+//     info.textContent = `Arriving at ${arriving}`;
+//     departBtn.disabled = false;
+//     arriveBtn.disabled = true;
+//   }
+
+//   return {
+//     depart,
+//     arrive,
+//   };
+// }
+
+// let result = solve();
