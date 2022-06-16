a1976470d56287462a1bfe3bccdf33a55d6b801c
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-06-16 12:45:54 +0300

    solving with async/await

--- 1. Asynchronous Programming/2. Asynchronous Programming - Exercise/01.Bus-Stop/app.js
+++ 1. Asynchronous Programming/2. Asynchronous Programming - Exercise/01.Bus-Stop/app.js
@@ -1,33 +1,66 @@
-function getInfo() {
+async function getInfo() {
   const inputId = document.getElementById("stopId");
   const stopName = document.getElementById("stopName");
   const busList = document.getElementById("buses");
 
   const url = "http://localhost:3030/jsonstore/bus/businfo";
 
-  stopName.innerHTML = "Loading...";
+  stopName.innerHTML = "Loading ...";
   busList.innerHTML = "";
 
-  fetch(`${url}/${inputId.value}`)
-    .then((response) => {
-      if (response.status !== 200 || inputId.value == "") {
-        throw Error();
-      }
+  try {
+    const response = await fetch(`${url}/${inputId.value}`);
 
-      return response.json();
-    })
-    .then((data) => {
-      stopName.textContent = data.name;
+    if (response.status !== 200 || inputId.value == "") {
+      throw new Error();
+    }
 
-      Object.entries(data.buses).forEach((b) => {
-        const liElement = document.createElement("li");
-        liElement.textContent = `Bus ${b[0]} arrives in ${b[1]} minutes`;
+    const data = await response.json();
 
-        console.log(liElement.textContent);
-        busList.appendChild(liElement);
-      });
-    })
-    .catch((err) => {
-      stopName.textContent = `${err}`;
+    stopName.textContent = data.name;
+
+    Object.entries(data.buses).forEach((b) => {
+      const liElement = document.createElement("li");
+
+      liElement.textContent = `Bus ${b[0]} arrives in ${b[1]} minutes`;
+
+      busList.appendChild(liElement);
     });
+  } catch (err) {
+    stopName.textContent = `${err}`;
+  }
 }
+
+// function getInfo() {
+//   const inputId = document.getElementById("stopId");
+//   const stopName = document.getElementById("stopName");
+//   const busList = document.getElementById("buses");
+
+//   const url = "http://localhost:3030/jsonstore/bus/businfo";
+
+//   stopName.innerHTML = "Loading...";
+//   busList.innerHTML = "";
+
+//   fetch(`${url}/${inputId.value}`)
+//     .then((response) => {
+//       if (response.status !== 200 || inputId.value == "") {
+//         throw Error();
+//       }
+
+//       return response.json();
+//     })
+//     .then((data) => {
+//       stopName.textContent = data.name;
+
+//       Object.entries(data.buses).forEach((b) => {
+//         const liElement = document.createElement("li");
+//         liElement.textContent = `Bus ${b[0]} arrives in ${b[1]} minutes`;
+
+//         console.log(liElement.textContent);
+//         busList.appendChild(liElement);
+//       });
+//     })
+//     .catch((err) => {
+//       stopName.textContent = `${err}`;
+//     });
+// }
