b9de6cae35da84b0d638f3e0c3893c2e236f73b4
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-06-27 13:24:24 +0300

    adding style to the clicked button on navigation

--- 2. Data and Authentication/05.Fisher-Game/src/app.js
+++ 2. Data and Authentication/05.Fisher-Game/src/app.js
@@ -15,7 +15,11 @@ const routes = {
 navigation.addEventListener("click", (e) => {
   e.preventDefault();
   if (e.target.tagName == "A") {
+    document.querySelector(".active").classList.remove("active");
+
     const url = new URL(e.target);
+
+    e.target.classList.add("active");
 
     let showPage = routes[url.pathname];
     hideAll();
