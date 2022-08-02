d02d006b3ef90e08247bee899f041b3b9613b073
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-02 21:04:43 +0300

    add home page

--- Exam Preparation/5. Exam/src/api/data.js
+++ Exam Preparation/5. Exam/src/api/data.js
@@ -1,10 +1,14 @@
 import * as api from "./api.js";
 import * as users from "./users.js";
 
-const endpoints = {};
+const endpoints = {
+  getAll: "/data/books?sortBy=_createdOn%20desc",
+};
 
 export const login = users.login;
 export const register = users.register;
 export const logout = users.logout;
 
-
+export const getAll = async () => {
+  return api.getRequest(endpoints.getAll);
+};
--- Exam Preparation/5. Exam/src/views/home.js
+++ Exam Preparation/5. Exam/src/views/home.js
@@ -1,37 +1,36 @@
+import { getAll } from "../api/data.js";
 import { html, until } from "../lib.js";
 
-const homeTemplate = () => html`
+const homeTemplate = (tempalte) => html`
+  <section id="dashboard-page" class="dashboard">
+    <h1>Dashboard</h1>
+    <ul class="other-books-list">
+      ${until(tempalte, html`<p>Loading &hellip;</p>`)}
+    </ul>
+  </section>
+`;
 
-        <section id="dashboard-page" class="dashboard">
-<h1>Dashboard</h1>
-<!-- Display ul: with list-items for All books (If any) -->
-<ul class="other-books-list">
-    <li class="otherBooks">
-        <h3>A Court of Thorns and Roses</h3>
-        <p>Type: Fiction</p>
-        <p class="img"><img src="./images/book1.png"></p>
-        <a class="button" href="#">Details</a>
-    </li>
+const bookTemplate = (book) => html`
+  <li class="otherBooks">
+    <h3>${book.title}</h3>
+    <p>Type: ${book.type}</p>
+    <p class="img"><img src=${book.imageUrl} /></p>
+    <a class="button" href="/details/${book._id}">Details</a>
+  </li>
+`;
 
-    <li class="otherBooks">
-        <h3>Outlander</h3>
-        <p>Type: Other</p>
-        <p class="img"><img src="/images/book2.png"></p>
-        <a class="button" href="#">Details</a>
-    </li>
-
-    <li class="otherBooks">
-        <h3>To Kill a Mockingbird</h3>
-        <p>Type: Classic</p>
-        <p class="img"><img src="/images/book3.png"></p>
-        <a class="button" href="#">Details</a>
-    </li>
-</ul>
-<!-- Display paragraph: If there are no books in the database -->
-<p class="no-books">No books in database!</p>
-</section>
+const noBooksTemplate = () => html`
+  <p class="no-books">No books in database!</p>
 `;
 
 export const homePage = (ctx) => {
-  ctx.render(homeTemplate());
+  ctx.render(homeTemplate(getBooks()));
+
+  async function getBooks() {
+    const books = await getAll();
+
+    return books.length > 0
+      ? books.map((b) => bookTemplate(b))
+      : noBooksTemplate();
+  }
 };
