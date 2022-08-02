c103ba9f8b7cdcd3db74fe5de030b6e4ca80d464
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-02 20:54:56 +0300

    add register functionality

--- Exam Preparation/5. Exam/src/views/register.js
+++ Exam Preparation/5. Exam/src/views/register.js
@@ -1,34 +1,43 @@
 import { register } from "../api/data.js";
 import { html } from "../lib.js";
 
-const registerTemplate = () => html`
-<!-- Register Page ( Only for Guest users ) -->
-<section id="register-page" class="register">
-            <form id="register-form" action="" method="">
-                <fieldset>
-                    <legend>Register Form</legend>
-                    <p class="field">
-                        <label for="email">Email</label>
-                        <span class="input">
-                            <input type="text" name="email" id="email" placeholder="Email">
-                        </span>
-                    </p>
-                    <p class="field">
-                        <label for="password">Password</label>
-                        <span class="input">
-                            <input type="password" name="password" id="password" placeholder="Password">
-                        </span>
-                    </p>
-                    <p class="field">
-                        <label for="repeat-pass">Repeat Password</label>
-                        <span class="input">
-                            <input type="password" name="confirm-pass" id="repeat-pass" placeholder="Repeat Password">
-                        </span>
-                    </p>
-                    <input class="button submit" type="submit" value="Register">
-                </fieldset>
-            </form>
-        </section>
+const registerTemplate = (onSubmit) => html`
+  <section id="register-page" class="register">
+    <form @submit=${onSubmit} id="register-form" action="" method="">
+      <fieldset>
+        <legend>Register Form</legend>
+        <p class="field">
+          <label for="email">Email</label>
+          <span class="input">
+            <input type="text" name="email" id="email" placeholder="Email" />
+          </span>
+        </p>
+        <p class="field">
+          <label for="password">Password</label>
+          <span class="input">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+            />
+          </span>
+        </p>
+        <p class="field">
+          <label for="repeat-pass">Repeat Password</label>
+          <span class="input">
+            <input
+              type="password"
+              name="confirm-pass"
+              id="repeat-pass"
+              placeholder="Repeat Password"
+            />
+          </span>
+        </p>
+        <input class="button submit" type="submit" value="Register" />
+      </fieldset>
+    </form>
+  </section>
 `;
 
 export const registerPage = (ctx) => {
@@ -41,8 +50,7 @@ export const registerPage = (ctx) => {
 
     const email = formData.get("email").trim();
     const password = formData.get("password").trim();
-    
-    //const rePass = formData.get("").trim(); 
+    const rePass = formData.get("confirm-pass").trim();
 
     if (email == "" || password == "" || password !== rePass) {
       return alert("All fields required!");
@@ -50,7 +58,7 @@ export const registerPage = (ctx) => {
 
     await register(email, password);
 
-    // ctx.navAction(ctx.userData());
-    // ctx.page.redirect();
+    ctx.navAction(ctx.userData());
+    ctx.page.redirect("/");
   }
 };
