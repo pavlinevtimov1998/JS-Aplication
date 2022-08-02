79119aa7352a24ed3e65403f6df42db447659570
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-02 22:10:45 +0300

    add like and delete functionality

--- Exam Preparation/5. Exam/src/api/data.js
+++ Exam Preparation/5. Exam/src/api/data.js
@@ -5,6 +5,11 @@ const endpoints = {
   getAll: "/data/books?sortBy=_createdOn%20desc",
   getOne: "/data/books/",
   create: "/data/books",
+  like: "/data/likes",
+  countLikes: (bookId) =>
+    `/data/likes?where=bookId%3D%22${bookId}%22&distinct=_ownerId&count`,
+  specificUserLike: (bookId, userId) =>
+    `/data/likes?where=bookId%3D%22${bookId}%22%20and%20_ownerId%3D%22${userId}%22&count`,
 };
 
 export const login = users.login;
@@ -25,4 +30,22 @@ export const createBook = (book) => {
 
 export const editBook = (id, book) => {
   return api.putRequest(endpoints.getOne + id, book);
+};
+
+export const deleteBook = (bookId) => {
+  return api.delRequest(endpoints.getOne + bookId);
+};
+
+// Likes
+
+export const createLike = (data) => {
+  return api.postRequest(endpoints.like, data);
+};
+
+export const getLikes = (bookId) => {
+  return api.getRequest(endpoints.countLikes(bookId));
+};
+
+export const getSpecifikLike = (bookId, userId) => {
+  return api.getRequest(endpoints.specificUserLike(bookId, userId));
 };
--- Exam Preparation/5. Exam/src/views/details.js
+++ Exam Preparation/5. Exam/src/views/details.js
@@ -1,5 +1,11 @@
-import { getOne } from "../api/data.js";
-import { html, until } from "../lib.js";
+import {
+  createLike,
+  deleteBook,
+  getLikes,
+  getOne,
+  getSpecifikLike,
+} from "../api/data.js";
+import { html, until, nothing } from "../lib.js";
 
 const detailsTemplate = (template) => html`
   <section id="details-page" class="details">
@@ -7,24 +13,41 @@ const detailsTemplate = (template) => html`
   </section>
 `;
 
-const bookTemplate = (book) => html`
+const bookTemplate = (
+  book,
+  userId,
+  onLike,
+  specificLike,
+  likes,
+  onDelete
+) => html`
   <div class="book-information">
     <h3>${book.title}</h3>
     <p class="type">Type: ${book.type}</p>
     <p class="img"><img src=${book.imageUrl} /></p>
     <div class="actions">
       <!-- Edit/Delete buttons ( Only for creator of this book )  -->
-      <a class="button" href="/edit/${book._id}">Edit</a>
-      <a class="button" href="#">Delete</a>
+      ${userId == book._ownerId
+        ? html`<a class="button" href="/edit/${book._id}">Edit</a>
+            <a @click=${onDelete} class="button" href="javascript:void(0)"
+              >Delete</a
+            >`
+        : nothing}
 
       <!-- Bonus -->
       <!-- Like button ( Only for logged-in users, which is not creators of the current book ) -->
-      <a class="button" href="#">Like</a>
+      ${userId && userId !== book._ownerId && specificLike == 0
+        ? html`
+            <a @click=${onLike} class="button" href="javascript:void(0)"
+              >Like</a
+            >
+          `
+        : nothing}
 
       <!-- ( for Guests and Users )  -->
       <div class="likes">
         <img class="hearts" src="/images/heart.png" />
-        <span id="total-likes">Likes: 0</span>
+        <span id="total-likes">Likes: ${likes}</span>
       </div>
       <!-- Bonus -->
     </div>
@@ -36,13 +59,34 @@ const bookTemplate = (book) => html`
 `;
 
 export const detailsPage = (ctx) => {
+  const userId = ctx.userData() ? ctx.userData().id : undefined;
+  const bookId = ctx.params.id;
+
   ctx.render(detailsTemplate(getBook()));
 
   async function getBook() {
-    const bookId = ctx.params.id;
+    let specificLike = null;
 
-    const book = await getOne(bookId);
+    const [book, likes] = await Promise.all([getOne(bookId), getLikes(bookId)]);
 
-    return bookTemplate(book);
+    if (userId && userId !== bookId) {
+      specificLike = await getSpecifikLike(bookId, userId);
+    }
+
+    console.log(specificLike, likes);
+
+    return bookTemplate(book, userId, onLike, specificLike, likes, onDelete);
+  }
+
+  async function onLike() {
+    const data = await createLike({ bookId });
+    console.log(data);
+    ctx.page.redirect("/details/" + bookId);
+  }
+
+  async function onDelete() {
+    await deleteBook(bookId);
+
+    ctx.page.redirect("/");
   }
 };
