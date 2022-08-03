293e71d904da87ef8a27d4d1b4a2fd120e866992
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-03 13:56:21 +0300

    add catalog functionlaity

--- Exam Preparation/6. Exam/Pet Care/src/api/data.js
+++ Exam Preparation/6. Exam/Pet Care/src/api/data.js
@@ -1,10 +1,14 @@
 import * as api from "./api.js";
 import * as users from "./users.js";
 
-const endpoints = {};
+const endpoints = {
+  getAll: "/data/pets?sortBy=_createdOn%20desc&distinct=name",
+};
 
 export const login = users.login;
 export const register = users.register;
 export const logout = users.logout;
 
-
+export const getAll = async () => {
+  return api.getRequest(endpoints.getAll);
+};
