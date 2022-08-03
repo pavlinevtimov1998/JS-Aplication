b0ec4d1292c9ce1faa83f3e58be880f5d7c69509
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-03 13:47:14 +0300

    add login functionality

--- Exam Preparation/6. Exam/Pet Care/src/views/login.js
+++ Exam Preparation/6. Exam/Pet Care/src/views/login.js
@@ -1,30 +1,41 @@
 import { login } from "../api/data.js";
 import { html } from "../lib.js";
 
-const loginTemlate = () => html`
- <!--Login Page-->
- <section id="loginPage">
-            <form class="loginForm">
-                <img src="./images/logo.png" alt="logo" />
-                <h2>Login</h2>
+const loginTemlate = (onSubmit) => html`
+  <section id="loginPage">
+    <form @submit=${onSubmit} class="loginForm">
+      <img src="./images/logo.png" alt="logo" />
+      <h2>Login</h2>
 
-                <div>
-                    <label for="email">Email:</label>
-                    <input id="email" name="email" type="text" placeholder="steven@abv.bg" value="">
-                </div>
+      <div>
+        <label for="email">Email:</label>
+        <input
+          id="email"
+          name="email"
+          type="text"
+          placeholder="steven@abv.bg"
+          value=""
+        />
+      </div>
 
-                <div>
-                    <label for="password">Password:</label>
-                    <input id="password" name="password" type="password" placeholder="********" value="">
-                </div>
+      <div>
+        <label for="password">Password:</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="********"
+          value=""
+        />
+      </div>
 
-                <button class="btn" type="submit">Login</button>
+      <button class="btn" type="submit">Login</button>
 
-                <p class="field">
-                    <span>If you don't have profile click <a href="#">here</a></span>
-                </p>
-            </form>
-        </section>
+      <p class="field">
+        <span>If you don't have profile click <a href="#">here</a></span>
+      </p>
+    </form>
+  </section>
 `;
 
 export const loginPage = (ctx) => {
@@ -45,6 +56,6 @@ export const loginPage = (ctx) => {
     await login(email, password);
 
     ctx.navAction(ctx.userData());
-    ctx.page.redirect("");
+    ctx.page.redirect("/");
   }
 };
