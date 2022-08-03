1dd8334cc38fa5726876c2b9326dc852993c48b1
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-03 13:45:33 +0300

    Update home.js

--- Exam Preparation/6. Exam/Pet Care/src/views/home.js
+++ Exam Preparation/6. Exam/Pet Care/src/views/home.js
@@ -1,17 +1,18 @@
 import { html, until } from "../lib.js";
 
 const homeTemplate = () => html`
-<!--Welcome Page-->
-<section class="welcome-content">
-            <article class="welcome-content-text">
-                <h1>We Care</h1>
-                <h1 class="bold-welcome">Your Pets</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
-            </article>
-            <article class="welcome-content-image">
-                <img src="./images/header-dog.png" alt="dog">
-            </article>
-        </section>
+  <section class="welcome-content">
+    <article class="welcome-content-text">
+      <h1>We Care</h1>
+      <h1 class="bold-welcome">Your Pets</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.
+      </p>
+    </article>
+    <article class="welcome-content-image">
+      <img src="/images/header-dog.png" alt="dog" />
+    </article>
+  </section>
 `;
 
 export const homePage = (ctx) => {
