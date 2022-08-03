e0fa827b0266edcd3bae818840c253182c461b51
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-03 14:24:17 +0300

    add create functionality

--- Exam Preparation/6. Exam/Pet Care/src/api/data.js
+++ Exam Preparation/6. Exam/Pet Care/src/api/data.js
@@ -4,20 +4,25 @@ import * as users from "./users.js";
 const endpoints = {
   getAll: "/data/pets?sortBy=_createdOn%20desc&distinct=name",
   getOne: "/data/pets/",
+  create: "/data/pets",
 };
 
 export const login = users.login;
 export const register = users.register;
 export const logout = users.logout;
 
-export const getAll = async () => {
+export const getAll = () => {
   return api.getRequest(endpoints.getAll);
 };
 
-export const getOne = async (id) => {
+export const getOne = (id) => {
   return api.getRequest(endpoints.getOne + id);
 };
 
-export const editAnimal = async (id, data) => {
+export const createAnimal = (data) => {
+  return api.postRequest(endpoints.create, data);
+};
+
+export const editAnimal = (id, data) => {
   return api.putRequest(endpoints.getOne + id, data);
 };
