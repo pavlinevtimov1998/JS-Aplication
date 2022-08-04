5c14f7620606afa8a9d73e032331fe01bc9472b8
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-04 13:29:10 +0300

    add login functionality

--- Exam Preparation/7. Exam/CarTube/src/views/login.js
+++ Exam Preparation/7. Exam/CarTube/src/views/login.js
@@ -1,29 +1,26 @@
 import { login } from "../api/data.js";
 import { html } from "../lib.js";
 
-const loginTemlate = () => html`
-<!-- Login Page -->
-<section id="login">
-            <div class="container">
-                <form id="login-form" action="#" method="post">
-                    <h1>Login</h1>
-                    <p>Please enter your credentials.</p>
-                    <hr>
+const loginTemlate = (onSubmit) => html`
+  <section id="login">
+    <div class="container">
+      <form @submit=${onSubmit} id="login-form" action="#" method="post">
+        <h1>Login</h1>
+        <p>Please enter your credentials.</p>
+        <hr />
 
-                    <p>Username</p>
-                    <input placeholder="Enter Username" name="username" type="text">
+        <p>Username</p>
+        <input placeholder="Enter Username" name="username" type="text" />
 
-                    <p>Password</p>
-                    <input type="password" placeholder="Enter Password" name="password">
-                    <input type="submit" class="registerbtn" value="Login">
-                </form>
-                <div class="signin">
-                    <p>Dont have an account?
-                        <a href="#">Sign up</a>.
-                    </p>
-                </div>
-            </div>
-        </section>
+        <p>Password</p>
+        <input type="password" placeholder="Enter Password" name="password" />
+        <input type="submit" class="registerbtn" value="Login" />
+      </form>
+      <div class="signin">
+        <p>Dont have an account? <a href="/register">Sign up</a>.</p>
+      </div>
+    </div>
+  </section>
 `;
 
 export const loginPage = (ctx) => {
@@ -34,16 +31,16 @@ export const loginPage = (ctx) => {
 
     const formData = new FormData(e.target);
 
-    const email = formData.get("email").trim();
+    const username = formData.get("username").trim();
     const password = formData.get("password").trim();
 
-    if (email == "" || password == "") {
+    if (username == "" || password == "") {
       return alert("All fields required!");
     }
 
-    await login(email, password);
+    await login(username, password);
 
     ctx.navAction(ctx.userData());
-    ctx.page.redirect("");
+    ctx.page.redirect("/catalog");
   }
 };
